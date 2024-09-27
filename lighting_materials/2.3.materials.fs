1e8e60072df29a1ef354c180256ac719ec06c4e0
ysouyno <ysouyno@163.com>
2024-09-27 17:12:17 +0800

    proj lighting_materials added based on lighting_basic_lighting

---
+++ lighting_materials/2.3.materials.fs
@@ -0,0 +1,42 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 Normal;
+in vec3 FragPos;
+
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+void main()
+{
+  // amibent
+  float ambientStrength = 0.1;
+  vec3 ambient = ambientStrength * lightColor;
+
+  // diffuse
+  // 因为只关心方向向量的方向，所以这里先进行标准化，即 normalize
+  // norm 是书中所说的法向量，垂直于立面体表面
+  vec3 norm = normalize(Normal);
+  vec3 lightDir = normalize(lightPos - FragPos);
+
+  // 将法向量和方向向量进行点乘，会得到它们之前夹角的余弦值
+  // 角度越大，余弦值越小
+  float diff = max(dot(norm, lightDir), 0.0);
+  vec3 diffuse = diff * lightColor;
+
+  // specular
+  float specularStrength = 0.5;
+  // FragPos 即光线入射点，viewDir 相当于光线入射方向
+  vec3 viewDir = normalize(viewPos - FragPos);
+  // reflectDir 即光线的反射方向
+  vec3 reflectDir = reflect(-lightDir, norm);
+  // 将观察向量和反射向量进行点乘，会得到它们之前夹角的余弦值
+  // 32 表示反光度（Shininess）
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+  vec3 specular = specularStrength * spec * lightColor;
+
+  vec3 result = (ambient + diffuse + specular) * objectColor;
+  FragColor = vec4(result, 1.0);
+}
---
+++ lighting_materials/2.3.materials.vs
@@ -0,0 +1,21 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+
+out vec3 FragPos;
+out vec3 Normal;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+  FragPos = vec3(model * vec4(aPos, 1.0));
+
+  // 因为例子中没有对立方体进行不等比缩放，所以这里的法线矩阵（Normal Matrix）没有必要
+  // Normal = mat3(transpose(inverse(model))) *aNormal;
+  Normal = aNormal;
+
+  gl_Position = projection * view * vec4(aPos, 1.0);
+}
