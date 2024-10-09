c8567ecf75732966ddd0b8495f4e75ba4189c40f
ysouyno <ysouyno@163.com>
2024-10-09 15:29:07 +0800

    几何着色器-造几个房子

--- advanced_opengl_geometry_shader/main.cpp
+++ advanced_opengl_geometry_shader/main.cpp
@@ -50,10 +50,10 @@ int main() {
   // set up vertex data (and buffer(s)) and configure vertex attributes
   // ------------------------------------------------------------------
   float points[] = {
-      -0.5f,  0.5f, // top-left
-       0.5f,  0.5f, // top-right
-       0.5f, -0.5f, // bottom-right
-      -0.5f, -0.5f  // bottom-left
+      -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // top-left
+       0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // top-right
+       0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // bottom-right
+      -0.5f, -0.5f, 1.0f, 1.0f, 0.0f  // bottom-left
   };
   unsigned int VBO, VAO;
   glGenBuffers(1, &VBO);
@@ -62,7 +62,9 @@ int main() {
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(points), &points, GL_STATIC_DRAW);
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
   glBindVertexArray(0);
 
   // render loop
