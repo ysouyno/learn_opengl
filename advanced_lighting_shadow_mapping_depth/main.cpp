7c9c7ba59cd58c2b098e452b4fead949b95cc588
ysouyno <ysouyno@163.com>
2024-10-13 12:15:05 +0800

    update README.org

--- advanced_lighting_shadow_mapping_depth/main.cpp
+++ advanced_lighting_shadow_mapping_depth/main.cpp
@@ -163,6 +163,27 @@ int main() {
   unsigned int cubeTexture = loadTexture("wood.png");
   unsigned int floorTexture = loadTexture("wood.png");
 
+  // configure depth map FBO
+  // -----------------------
+  const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+  unsigned int depthMapFBO;
+  glGenFramebuffers(1, &depthMapFBO);
+  // create depth texture
+  unsigned int depthMap;
+  glGenTextures(1, &depthMap);
+  glBindTexture(GL_TEXTURE_2D, depthMap);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FALSE, NULL);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // attach depth texture as FBO's depth buffer
+  glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+  glDrawBuffer(GL_NONE);
+  glReadBuffer(GL_NONE);
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
   // shader configuration
   // --------------------
   shader.use();
