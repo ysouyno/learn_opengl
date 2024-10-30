75c7c0bc1e0df8bbd4846ab7631812f8030c0484
ysouyno <ysouyno@163.com>
2024-10-30 22:46:39 +0800

    解决提取亮度中遇到的疑问

--- advanced_lighting_bloom/5.7.bloom.fs
+++ advanced_lighting_bloom/5.7.bloom.fs
@@ -1,6 +1,7 @@
 # version 330 core
 
-out vec4 FragColor;
+layout (location = 0) out vec4 FragColor;
+layout (location = 1) out vec4 BrightColor;
 
 in VS_OUT {
   vec3 FragPos;
@@ -13,8 +14,9 @@ struct Light {
   vec3 Color;
 };
 
-uniform Light lights[16];
+uniform Light lights[4];
 uniform sampler2D diffuseTexture;
+uniform vec3 viewPos;
 
 void main() {
   vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb;
@@ -23,7 +25,8 @@ void main() {
   vec3 ambient = 0.0 * color;
   // lighting
   vec3 lighting = vec3(0.0);
-  for (int i = 0; i < 16; ++i) {
+  vec3 viewDir = normalize(viewPos - fs_in.FragPos);
+  for (int i = 0; i < 4; ++i) {
     // diffuse
     vec3 lightDir = normalize(lights[i].Position - fs_in.FragPos);
     float diff = max(dot(lightDir, normal), 0.0);
@@ -34,5 +37,14 @@ void main() {
     result *= 1.0 / (distance * distance);
     lighting += result;
   }
-  FragColor = vec4(ambient + lighting, 1.0);
+
+  vec3 result = ambient + lighting;
+  // check whether result is higher than some threshold
+  float brightness = dot(result, vec3(0.2126, 0.7152, 0.0722));
+  if (brightness > 1.0)
+    BrightColor = vec4(result, 1.0);
+  else
+    BrightColor = vec4(0.0, 0.0, 0.0, 1.0);
+
+  FragColor = vec4(result, 1.0);
 }
