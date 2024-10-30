b5a331056b881ee8763e3c09e072e9514348bc49
ysouyno <ysouyno@163.com>
2024-10-30 22:01:00 +0800

    泛光中的提取亮度

--- advanced_lighting_bloom/5.7.lighting.fs
+++ advanced_lighting_bloom/5.7.lighting.fs
@@ -1,6 +1,9 @@
 # version 330 core
 
-out vec4 FragColor;
+// location 0 对应 GL_COLOR_ATTACHMENT0
+// location 1 对应 GL_COLOR_ATTACHMENT1
+layout (location = 0) out vec4 FragColor;
+layout (location = 1) out vec4 BrightColor;
 
 in VS_OUT {
   vec3 FragPos;
@@ -12,4 +15,9 @@ uniform vec3 lightColor;
 
 void main() {
   FragColor = vec4(lightColor, 1.0);
+  float brightness = dot(FragColor.rgb, vec3(0.2126, 0.7152, 0.0722));
+  if (brightness > 1.0)
+    BrightColor = vec4(FragColor.rgb, 1.0);
+  else
+    BrightColor = vec4(0.0, 0.0, 0.0, 1.0);
 }
--- advanced_lighting_bloom/main.cpp
+++ advanced_lighting_bloom/main.cpp
@@ -84,22 +84,29 @@ int main() {
   // ------------------------------------
   unsigned int hdrFBO;
   glGenFramebuffers(1, &hdrFBO);
+  glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
   // create floating point color buffer
-  unsigned int colorBuffer;
-  glGenTextures(1, &colorBuffer);
-  glBindTexture(GL_TEXTURE_2D, colorBuffer);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  unsigned int colorBuffers[2];
+  glGenTextures(2, colorBuffers);
+  for (unsigned int i = 0; i < 2; ++i) {
+    glBindTexture(GL_TEXTURE_2D, colorBuffers[i]);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    // attach texture to framebuffer
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, colorBuffers[i], 0);
+  }
   // create depth buffer
   unsigned int rboDepth;
   glGenRenderbuffers(1, &rboDepth);
   glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
   glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, SCR_WIDTH, SCR_HEIGHT);
-  // attach buffers
-  glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBuffer, 0);
   glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
+  // tell OpenGL which color attachments we'll use (of this framebuffer) for rendering
+  unsigned int attachments[2] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
+  glDrawBuffers(2, attachments);
   if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
     std::cout << "Framebuffer not complete!" << std::endl;
   glBindFramebuffer(GL_FRAMEBUFFER, 0);
@@ -121,6 +128,8 @@ int main() {
 
   shader.use();
   shader.set_int("diffuseTexture", 0);
+  shaderBlur.use();
+  shaderBlur.set_int("hdrBuffer", 0);
 
   // render loop
   // -----------
@@ -233,7 +242,7 @@ int main() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     shaderBlur.use();
     glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, colorBuffer);
+    glBindTexture(GL_TEXTURE_2D, colorBuffers[1]);
     renderQuad();
 
     // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
